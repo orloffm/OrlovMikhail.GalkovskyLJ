941c436e553483fa23cbd32f3a81d0c1db764ebe
orloffm <orloffm@gmail.com>
2015-08-31 13:03:49 +0200

    bookmaker avoid existing files

--- book/bookmaker.ps1
+++ book/bookmaker.ps1
@@ -11,7 +11,10 @@ $dumps = dir -recurse -path $source -file -filter dump.xml
 Write-Host ("" + $dumps.Length + " found.")
 
 ForEach($dump in $dumps){
-    $dumpPath = $dump.FullName
-    Write-Host $dumpPath
-    Start-Process -Wait -NoNewWindow -FilePath "$scriptPath\..\bin\bookmaker.exe" -ArgumentList "/root=`"$scriptPath`" /source=`"$dumpPath`""
+	$fragmentPath = Join-Path $dump.Directory.FullName "fragment.asc"
+	if(!(Test-Path -Path $fragmentPath )){
+		$dumpPath = $dump.FullName
+		Write-Host $dumpPath
+		Start-Process -Wait -NoNewWindow -FilePath "$scriptPath\..\bin\bookmaker.exe" -ArgumentList "/root=`"$scriptPath`" /source=`"$dumpPath`""
+	}
 }
