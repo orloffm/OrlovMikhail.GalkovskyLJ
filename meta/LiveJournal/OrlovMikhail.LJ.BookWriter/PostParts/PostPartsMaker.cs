a58567cac5c6adfa5374d3de2f56a0da5417b564
orloffm <orloffm@gmail.com>
2015-09-01 23:01:18 +0200

    formatting over paragraphs

--- meta/LiveJournal/OrlovMikhail.LJ.BookWriter/PostParts/PostPartsMaker.cs
+++ meta/LiveJournal/OrlovMikhail.LJ.BookWriter/PostParts/PostPartsMaker.cs
@@ -51,10 +51,91 @@ namespace OrlovMikhail.LJ.BookWriter
 
             RemoveLineBreaksBeforeAndAfterFormatting(ret);
 
+            // Span formatting over paragraphs.
+            SpanFormattingOverParagraphs(ret);
+
             // Images must be on separate lines.
             EnsureImagesSurroundedWithNewParagraphs(ret);
 
             return ret.ToArray();
+        }
+
+        /// <summary>Makes sure formatting starts and ends inside
+        /// one paragraph.</summary>
+        private void SpanFormattingOverParagraphs(List<PostPartBase> items)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                ParagraphStartPart p1 = items[i] as ParagraphStartPart;
+                if (p1 == null)
+                    continue;
+
+                PostPartBase formattingStarter = (i < items.Count - 1 ? items[i + 1] : null);
+                if (formattingStarter == null)
+                    return;
+
+                PostPartBase needsToEndWith = null;
+                if (formattingStarter is ItalicStartPart)
+                    needsToEndWith = new ItalicEndPart();
+                else if (formattingStarter is BoldStartPart)
+                    needsToEndWith = new BoldEndPart();
+                else
+                    continue;
+
+                int p2Index = FindNextPart<ParagraphStartPart>(items, i);
+                if (p2Index <= i)
+                    return;
+
+                int precedingPartIndex = p2Index - 1;
+                PostPartBase precedingPart = items[precedingPartIndex];
+                if (precedingPart.GetType() == needsToEndWith.GetType())
+                {
+                    i = p2Index - 1;
+                    continue;
+                }
+
+                // Insert formatting ending part.
+                items.Insert(precedingPartIndex + 1, needsToEndWith);
+                p2Index++;
+
+                do
+                {
+                    // Now let's make sure next is the same starting.
+                    if (items[p2Index + 1] is ImagePart)
+                    {
+                        // Don't work with images. Go to next paragraph.
+                        p2Index = FindNextPart<ParagraphStartPart>(items, p2Index);
+                        if (p2Index < 0)
+                            return;
+
+                        continue;
+                    }
+
+                    break;
+                } while (true);
+
+                // Item after the next paragraph.
+                PostPartBase following = items[p2Index + 1];
+                if (following.GetType() != formattingStarter.GetType())
+                {
+                    // Formatting starter there.
+                    items.Insert(p2Index + 1, formattingStarter);
+                }
+
+                // Go to that paragraph next time.
+                i = p2Index - 1;
+            }
+        }
+
+        int FindNextPart<T>(List<PostPartBase> items, int start, bool reverse = false) where T : PostPartBase
+        {
+            int add = reverse ? -1 : +1;
+
+            for (int p = start + add; p < items.Count && p >= 0; p += add)
+                if (items[p] is T)
+                    return p;
+
+            return -1;
         }
 
         private void EnsureImagesSurroundedWithNewParagraphs(List<PostPartBase> items)
