c8b46cb3a6e878a3040e2e50fbf19dc2974219fd
orloffm <orloffm@gmail.com>
2015-09-04 11:19:18 +0200

    699 fix, clean all

--- book/clean.ps1
+++ book/clean.ps1
@@ -4,6 +4,12 @@
  
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
-"Removing all fragment.asc files from $source..."
-
-dir -path $source -recurse -include "fragment.asc" | %{ rm $_.FullName }
+IF($source -eq "."){
+	"Removing all .asc and output files..."
+	dir -path $scriptPath -recurse -filter "*.asc" | %{ rm $_.FullName }
+	dir -path $scriptPath -recurse -filter "*.pdf" | %{ rm $_.FullName }
+}
+else{
+	"Removing all fragment.asc files from $source..."
+	dir -path $source -recurse -include "fragment.asc" | %{ rm $_.FullName }
+}
