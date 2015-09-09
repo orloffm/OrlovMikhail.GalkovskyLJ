0320c57ccb8cecb19f33256f564114954515c4f8
orloffm <orloffm@gmail.com>
2015-09-09 17:01:36 +0200

    build to output

--- book/build.ps1
+++ book/build.ps1
@@ -5,35 +5,38 @@
  # This script builds all years.
 
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$outputPath = Join-Path $scriptPath "output"
 
-$years = dir -path $scriptPath -filter "*$filter*"
+$sourceAscs = dir -path $scriptPath -filter "*$filter*"
 
 $env:KINDLEGEN =  Join-Path $scriptPath 'resources\kindlegen.exe'
 
-ForEach($year in $years){
-	If([System.IO.Path]::GetExtension($year) -ne ".asc"){
+ForEach($sourceAsc in $sourceAscs){
+	If([System.IO.Path]::GetExtension($sourceAsc) -ne ".asc"){
 		continue
 	}
 
     " "
     "===================="
-    $year.Name
+    $sourceAsc.Name
     "===================="
     " "
-
-    $bookFile = ($scriptPath + "\" + $year.Name)
+	$nameWithoutExt = [System.IO.Path]::GetFileNameWithoutExtension($sourceAsc)
+    $bookFile = ($scriptPath + "\" + $sourceAsc.Name)
 
     # "Converting to HTML..."
     # & bundle exec asciidoctor $bookFile
 
     "Converting to EPub..."
-    & bundle exec asciidoctor-epub3 $bookFile
+    $outputEpub = Join-Path $outputPath ($nameWithoutExt + ".epub")
+    & bundle exec asciidoctor-epub3 $bookFile -o "$outputEpub$"
 
     # "Converting to Mobi (kf8)..."
     # & bundle exec asciidoctor-epub3 -a ebook-format=kf8 $bookFile
 
     "Converting to PDF..."
-    & bundle exec asciidoctor-pdf $bookFile -a pdf-style=resources/themes/galkovsky-theme.yml --trace
+    $outputPdf = Join-Path $outputPath ($nameWithoutExt + ".A4.pdf")
+    & bundle exec asciidoctor-pdf $bookFile -o "$outputPdf" -a pdf-style=resources/themes/galkovsky-theme.yml --trace
 }
 
 $outputPath = Join-Path $scriptPath "output"
@@ -44,13 +47,14 @@ else{
     #Remv ($outputPath + "\*") -Recurse
 }
 
+rm ($outputPath + "\*.pdfmarks")
 
-Write-Host "Moving files to output folder..."
-mv -Path ($scriptPath + "\*.html") -Destination $outputPath -Force
-rm ($scriptPath + "\*-kf8.epub")
-mv -Path ($scriptPath + "\*.epub") -Destination $outputPath -Force
-mv -Path ($scriptPath + "\*.mobi") -Destination $outputPath -Force
-rm ($scriptPath + "\*.pdfmarks")
-mv -Path ($scriptPath + "\*.pdf") -Destination $outputPath -Force
+#Write-Host "Moving files to output folder..."
+#mv -Path ($scriptPath + "\*.html") -Destination $outputPath -Force
+#rm ($scriptPath + "\*-kf8.epub")
+#mv -Path ($scriptPath + "\*.epub") -Destination $outputPath -Force
+#mv -Path ($scriptPath + "\*.mobi") -Destination $outputPath -Force
+#rm ($scriptPath + "\*.pdfmarks")
+#mv -Path ($scriptPath + "\*.pdf") -Destination $outputPath -Force
 
 Write-Host "Finished!"
